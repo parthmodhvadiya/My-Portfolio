7ff13daaab8c54bb15ef7da38629ce8f1dd75a0a
Harshil Kothiya <harshilkothiya5@gmail.com>
2025-03-06 20:14:30 +0530

    last

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Project.css";
-import ppsu from "../../assets/ppsu.jpg";
-import mbv from "../../assets/mbv.jpg";
-import hotel from "../../assets/hotel.png";
+import sport from "../../assets/sc.png";
+import datavis from "../../assets/datavis.png"
+import rul from "../../assets/RUL.png"
+import farm from "../../assets/farm.png"
+import ipl from "../../assets/ipl.png"
+import hotel1 from "../../assets/hotel1.png"
+import bangalore from "../../assets/home.png"
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
@@ -14,81 +18,53 @@ import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 const projects = [
   {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0HAORUOo06nzdHoEfuFzsul1hN2R7vdgsA&s",
+    image:rul,
     description:
       "Deepl learning Project to predict the Remaining Useful Life of Jet Engine.",
     link: "https://github.com/Harshilkothiya/Remaining-Useful-Life-RUL-of-Jet-Engine.git",
     title: "Remaining Useful Life of Jet Engine",
   },
   {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0HAORUOo06nzdHoEfuFzsul1hN2R7vdgsA&s",
+    image:farm,
     description:
       "An intelligent farming solution optimizing crop management through data-driven insights.",
     link: "https://github.com/Harshilkothiya/FarmSmart-Crop-Recommendation",
     title: "FarmSmart Crop Recommendation",
   },
   {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0HAORUOo06nzdHoEfuFzsul1hN2R7vdgsA&s",
+    image:ipl,
     description:
       "Predict the probability of win of Team based on current state.",
     link: "https://github.com/Harshilkothiya/IPL-win-probability",
     title: "IPL win probability",
   },
+  ,
   {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0HAORUOo06nzdHoEfuFzsul1hN2R7vdgsA&s",
+    image:hotel1,
+    description: "A MERN stack project for Hotel Booking.",
+    link: "https://github.com/Harshilkothiya/My-Hotel",
+    title: "Hotel Bookling App",
+  },
+  {
+    image:sport,
     description: "Classify the celebrities based on face.",
     link: "https://github.com/Harshilkothiya/Sports-Celebrity-Image-Classification",
     title: "Sports Celebrity Image Classification",
   },
   {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0HAORUOo06nzdHoEfuFzsul1hN2R7vdgsA&s",
+    image:datavis,
     description:
       "Data visualization of Road accident and crime in india on Power BI.",
     link: "https://github.com/Harshilkothiya/Data-visualization",
     title: "Data visualization",
   },
   {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0HAORUOo06nzdHoEfuFzsul1hN2R7vdgsA&s",
-    description: "Check the whether the given 2 question are duplicate or not.",
-    link: "https://github.com/Harshilkothiya/Duplicate-questions",
-    title: "Duplicate questions",
-  },
-  {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0HAORUOo06nzdHoEfuFzsul1hN2R7vdgsA&s",
-    description: "Predict the Next word.",
-    link: "https://github.com/Harshilkothiya/Next-Word-Prediction",
-    title: "Next Word Prediction",
-  },
-
-  {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0HAORUOo06nzdHoEfuFzsul1hN2R7vdgsA&s",
+    image:bangalore,
     description:
       " A machine learning model to estimate property prices based on location and features.",
     link: "https://github.com/Harshilkothiya/Bangalore-Home-Price-Prediction",
     title: "Bangalore Home Price Prediction",
-  },
-  {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0HAORUOo06nzdHoEfuFzsul1hN2R7vdgsA&s",
-    description: "Real time chat app using socket-io.",
-    link: "https://github.com/Harshilkothiya/ChatApp",
-    title: "Chat App",
-  },
-  {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0HAORUOo06nzdHoEfuFzsul1hN2R7vdgsA&s",
-    description: "A MERN stack project for Hotel Booking.",
-    link: "https://github.com/Harshilkothiya/My-Hotel",
-    title: "Hotel Bookling App",
-  },
+  }
   
 ];
 
